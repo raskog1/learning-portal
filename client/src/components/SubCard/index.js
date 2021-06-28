a31411784b4197bcb694967b02a4c70cc8544331
Ryan Skog <ryan.skog@outlook.com>
2021-06-28 13:50:59 -0700

    Transferring application to personal profile

--- client/src/components/SubCard/index.js
+++ client/src/components/SubCard/index.js
@@ -1,23 +1,26 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { Grid } from "@material-ui/core";
 import "./style.css";
 
 function SubCard({ alt, description, name, src }) {
   return (
     <Grid item xs={12} sm={3} md={6} className="wrap">
-      <div className="tile">
-        <img src={src} alt={alt} />
-        <div className="text">
-          {/* <h1>{name}</h1> */}
-          <p className="animate-text">{description} </p>
-          <h3 className="animate-text">Take the {name} assessment here</h3>
-          <div className="dots">
-            <span></span>
-            <span></span>
-            <span></span>
+      <Link to="/login">
+        <div className="tile">
+          <img src={src} alt={alt} />
+          <div className="text">
+            {/* <h1>{name}</h1> */}
+            <p className="animate-text">{description} </p>
+            <h3 className="animate-text">Take the {name} assessment here</h3>
+            <div className="dots">
+              <span></span>
+              <span></span>
+              <span></span>
+            </div>
           </div>
         </div>
-      </div>
+      </Link>
     </Grid>
   );
 }
